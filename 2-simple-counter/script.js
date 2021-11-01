fdaf36fc5ea53186d791be694f3d080d5a4bb72d
huy <okey222k@gmail.com>
2021-11-01 17:12:21 +0700

    setup

--- 2-simple-counter/script.js
+++ 2-simple-counter/script.js
@@ -1,29 +1,27 @@
+const value = document.querySelector("#value")
+const btns = document.querySelectorAll(".btn")
+const HEX = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 let count = 0;
 
-// select value and button
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
-
-// console.log(btns);
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        const styles = e.currentTarget.classList;
-        if(styles.contains('decrease')){
+btns.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        let hex = "#"
+        let style = e.target.classList
+        if(style.contains("decrease")){
             count--;
-        }else if(styles.contains('reset')){
-            count = 0;
-        }else{
+        }else if(style.contains("increase")){
             count++;
+        }else{
+            count = 0
         }
-        if(count >0){
-            value.style.color = 'green';
+        for(let i = 0; i < 6; i++){
+            hex += HEX[Math.floor(Math.random() * HEX.length)]
         }
-        if(count < 0){
-            value.style.color = 'red';
+        if(count <0 || count > 0){
+            value.style.color = hex
+        }else{
+            value.style.color = "black"
         }
-        if(count === 0){
-            value.style.color = 'yellow';
-        }
-        value.textContent = count
-    });
-});
+        value.textContent = count;
+    })
+})
