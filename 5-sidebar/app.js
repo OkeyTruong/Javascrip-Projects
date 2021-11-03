c796e5b045277bddb6c0b4d2086346919c13d808
huy <okey222k@gmail.com>
2021-11-03 19:54:49 +0700

    done

---
+++ 5-sidebar/app.js
@@ -0,0 +1,15 @@
+const sidebarToggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+sidebarToggleBtn.addEventListener('click', () => {
+    if(sidebar.classList.contains("show-sidebar")){
+        sidebar.classList.remove("show-sidebar");
+    }else{
+        sidebar.classList.add("show-sidebar");
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove("show-sidebar");
+})
