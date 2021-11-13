61453c14c95c065deb9ed3a9eb5ce8fa52a6776e
huy <okey222k@gmail.com>
2021-11-13 19:34:29 +0700

    done

--- 9-video/app.js
+++ 9-video/app.js
@@ -1,17 +1,19 @@
-const switchBtn = document.querySelector('.switch-btn');
-const video = document.querySelector('.video-container');
-switchBtn.addEventListener('click', () => {
-    if(switchBtn.classList.contains("slide")){
-        switchBtn.classList.remove("slide");
-        video.play();    
-    }else{
-        switchBtn.classList.add("slide");
-        video.pause();    
-    }
-})
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
 
-const preloader = document.querySelector('.preloader');
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
 
-window.addEventListener('load', () => {
-    preloader.classList.add("hide-preloader"); 
+// preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
 });
